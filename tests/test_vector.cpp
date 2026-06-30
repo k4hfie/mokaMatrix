f741d319cb28b0ffee92463a414c4ef360a7e11d
k4hfie <1adacchi@gmail.com>
2026-06-30 22:43:34 +0700

    added back

---
+++ tests/test_vector.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "vector.h"
+
+int main(){
+    VectorType vec1(2);
+    VectorType vec2(2);
+    std::cin >> vec1 >> vec2;
+    std::cout << vec1 << vec2 << "\n";
+    try {
+        double norm = vec1.vectorNorm();
+        double dotproduct = vec1.dotProduct(vec2);
+        std::cout << "norm: " << norm << "\ndot product: " << dotproduct << "\n";
+
+        VectorType result = vec1 + vec2;
+        std::cout << "addition vector: " << result << "\n";
+
+        result = vec1 - vec2;
+        std::cout << "subtraction vector: " << result << "\n";
+
+        result = vec1 * 10;
+        std::cout << "scalar multiplication vector: " << result << "\n";
+
+        result = vec1 / 10;
+        std::cout << "scalar division vector: " << result << "\n";
+
+        std::cout << vec1 << " == " << vec2 << ": " << (vec1 == vec2) << "\n";
+        std::cout << vec1 << " != " << vec2 << ": " << (vec1 != vec2) << "\n";
+
+        std::cout << "vec1[0]: " << vec1[0] << '\n';
+        std::cout << "vec2[0]: " << vec2[0] << '\n';
+    }
+
+    catch (const std::invalid_argument& e){
+        std::cout << "error: " << e.what();
+    }
+}
